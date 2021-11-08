e72977ec203852d6116a1c6658492c51b9e2bd80
Briannys <briannysmonserrate@gmail.com>
2021-11-08 11:44:16 -0500

    solving chart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "chartkick/chart.js"
 
-= require chartkick
-= require Chart.bundle
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
