bd2effd30ea0586462bcdf85219a3b9470adaae3
Briannys <briannysmonserrate@gmail.com>
2021-11-08 10:56:43 -0500

    add funtions create, show, edit and destroy, to calory

---
+++ app/controllers/calories_controller.rb
@@ -0,0 +1,56 @@
+class CaloriesController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @calories = Calory.where(user_id: current_user.id)
+  end
+
+  def new
+    @calory = Calory.new
+  end
+
+  def create
+    @calory = Calory.new(calory_params)
+    @calory.user_id = current_user.id
+    if @calory.valid?
+      @calory.save
+      flash[:success] = "Register yours calories!"
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @calory = Calory.find(params[:id])
+    if @calory.destroy
+      flash[:success] = "The to-do item was successfully destroyed."
+      redirect_to root_path
+    else
+      flash[:error] = "Something was wrong."
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @calory = Calory.find(params[:id])
+  end
+
+  def edit
+    @calory = Calory.find(params[:id])
+  end
+
+  def update
+    @calory = Calory.find(params[:id])
+    respond_to do |format|
+     if @calory.update(calory_params)
+       format.html { redirect_to @calory, notice: "Progress was successfully updated." }
+       format.json { render :show, status: :ok, location: @calory }
+     else
+       format.html { render :edit, status: :unprocessable_entity }
+       format.json { render json: @calory.errors, status: :unprocessable_entity }
+     end
+   end
+  end
+  private
+    def calory_params
+        params.require(:calory).permit(:burned_calories, :comment_burned,  :ingested_calories, :comment_ingested)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :calories
+  get 'calory/new', to:'calories#new'
+  post 'calories', to: 'calories#create'
+  get 'index', to: 'calories#index'
+#  get 'calories/:id/edit', to: 'calories#edit', as: 'edit_calory'
+#  patch 'calories', to: 'calories#update'
+  get 'calories/:id/show', to: 'calories#show', as: 'show_calory'
+  delete 'calory/:id', to: 'calories#destroy', as:'destroy_calory'
   root 'home#home'
 end
