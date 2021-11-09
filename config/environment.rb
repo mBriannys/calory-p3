99ca55590916f8bff9e2a4a52e3317e9d27fa38a
Briannys <briannysmonserrate@gmail.com>
2021-11-08 22:27:10 -0500

    try send email

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-      :address => 'smtp.sendgrid.net',
-      :port => '587',
-      :authentication => :plain,
-      :user_name => Rails.application.credentials.dig(:user_name),
-      :password => Rails.application.credentials.dig(:password),
-      :domain => 'heroku.com',
-      :enable_starttls_auto => true
-    }
-
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options ={:host => 'http://localhost:3000'}
